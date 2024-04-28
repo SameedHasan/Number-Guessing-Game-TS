7107b4603dcb0e484f84a05d7521179c86a3807e
Sameed Hasan <sameedh41@gmail.com>
2024-04-28 14:39:23 +0500

    version 1.0..3

--- index.js
+++ index.js
@@ -9,35 +9,33 @@ function isValidSingleDigit(input) {
     return regex.test(input);
 }
 // console.log("numberToGuess :>> ", numberToGuess);
-inquirer
-    .prompt([
-    {
-        name: "num",
-        message: "Guess the number (0-9): ",
-        type: "input",
-        validate: function (input) {
-            if (!isNaN(input) && isValidSingleDigit(input)) {
-                return true;
-            }
-            else {
-                return "Please enter a valid number";
-            }
-        },
-    },
-])
-    .then((answers) => {
-    if (Number(answers.num) === numberToGuess) {
-        console.log(`Congratulations! The number you have guessed is correct.`);
+// Main function to handle user input
+async function main() {
+    // Interactive menu
+    while (true) {
+        const answers = await inquirer.prompt([
+            {
+                name: "num",
+                message: "Guess the number (0-9): ",
+                type: "input",
+                validate: function (input) {
+                    if (!isNaN(input) && isValidSingleDigit(input)) {
+                        return true;
+                    }
+                    else {
+                        return "Please enter a valid number";
+                    }
+                },
+            },
+        ]);
+        if (Number(answers.num) === numberToGuess) {
+            console.log(`Congratulations! The number you have guessed is correct.`);
+            break;
+        }
+        else {
+            console.log(`Oops! The number you have guessed is incorrect.Try Again.`);
+        }
     }
-    else {
-        console.log(`Oops! The number you have guessed is incorrect.`);
-    }
-})
-    .catch((error) => {
-    if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-    }
-    else {
-        // Something else went wrong
-    }
-});
+}
+// Start the application
+main().catch(console.error);
--- index.ts
+++ index.ts
@@ -11,32 +11,34 @@ function isValidSingleDigit(input: string): boolean {
   return regex.test(input);
 }
 // console.log("numberToGuess :>> ", numberToGuess);
-inquirer
-  .prompt([
-    {
-      name: "num",
-      message: "Guess the number (0-9): ",
-      type: "input",
-      validate: function (input) {
-        if (!isNaN(input) && isValidSingleDigit(input)) {
-          return true;
-        } else {
-          return "Please enter a valid number";
-        }
+
+// Main function to handle user input
+async function main() {
+  // Interactive menu
+  while (true) {
+    const answers = await inquirer.prompt([
+      {
+        name: "num",
+        message: "Guess the number (0-9): ",
+        type: "input",
+        validate: function (input) {
+          if (!isNaN(input) && isValidSingleDigit(input)) {
+            return true;
+          } else {
+            return "Please enter a valid number";
+          }
+        },
       },
-    },
-  ])
-  .then((answers) => {
+    ]);
+
     if (Number(answers.num) === numberToGuess) {
       console.log(`Congratulations! The number you have guessed is correct.`);
+      break;
     } else {
-      console.log(`Oops! The number you have guessed is incorrect.`);
+      console.log(`Oops! The number you have guessed is incorrect.Try Again.`);
     }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+  }
+}
+
+// Start the application
+main().catch(console.error);
